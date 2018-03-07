343d4d76923420ee700b4a3ad2d37caf34a2f947
bashalir <bachir@sahali.fr>
2018-03-07 23:13:17 +0100

    Add resource files

--- app/src/main/java/com/oc/bashalir/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/oc/bashalir/moodtracker/controller/MainActivity.java
@@ -2,14 +2,29 @@ package com.oc.bashalir.moodtracker.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 
 import com.oc.bashalir.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mListMoodRecyclerView;
+    private Button mCommentButton;
+    private Button mHistoryButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListMoodRecyclerView = findViewById(R.id.activity_main_listMood_rv);
+        mCommentButton = findViewById(R.id.activity_main_comment_btn);
+        mHistoryButton = findViewById(R.id.activity_main_history_btn);
+
+        mListMoodRecyclerView.setLayoutManager (new LinearLayoutManager(this));
+      //  rv.setAdapter(new MyAdapter());
+
     }
 }
