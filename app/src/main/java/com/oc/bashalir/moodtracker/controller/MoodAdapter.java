c04ea181d73ab0336d42152bec747fd49aefa3fa
bashalir <bachir@sahali.fr>
2018-03-10 00:15:52 +0100

    Fix BackgroundColor

--- app/src/main/java/com/oc/bashalir/moodtracker/controller/MoodAdapter.java
+++ app/src/main/java/com/oc/bashalir/moodtracker/controller/MoodAdapter.java
@@ -1,9 +1,7 @@
 package com.oc.bashalir.moodtracker.controller;
 
-import android.content.res.Resources;
-import android.graphics.drawable.ColorDrawable;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.res.ResourcesCompat;
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.util.Pair;
@@ -18,7 +16,7 @@ import com.oc.bashalir.moodtracker.R;
 import java.util.Arrays;
 import java.util.List;
 
-import static android.view.View.SCROLLBAR_POSITION_DEFAULT;
+
 
 
 /**
@@ -28,12 +26,12 @@ import static android.view.View.SCROLLBAR_POSITION_DEFAULT;
 public class MoodAdapter extends RecyclerView.Adapter<MoodAdapter.MoodViewHolder> {
 
 
-    private final List<Pair<Integer,Integer>> listMood = Arrays.asList (
-            Pair.create(R.drawable.smiley_sad, R.color.faded_red),
-            Pair.create(R.drawable.smiley_disappointed,  R.color.warm_grey),
-            Pair.create(R.drawable.smiley_normal, R.color.light_sage),
-            Pair.create(R.drawable.smiley_happy,  R.color.cornflower_blue_65),
-            Pair.create(R.drawable.smiley_super_happy, R.color.banana_yellow)
+    private final List<Pair<Integer,String>> listMood = Arrays.asList (
+            Pair.create(R.drawable.smiley_sad, "#FFDE3C50"),
+            Pair.create(R.drawable.smiley_disappointed,  "#ff9b9b9b"),
+            Pair.create(R.drawable.smiley_normal, "#a5468ad9"),
+            Pair.create(R.drawable.smiley_happy,  "#ffb8e986"),
+            Pair.create(R.drawable.smiley_super_happy, "#fff9ec4f")
             );
 
 
@@ -53,29 +51,29 @@ public class MoodAdapter extends RecyclerView.Adapter<MoodAdapter.MoodViewHolder
     @Override
     public void onBindViewHolder(MoodViewHolder holder, int position) {
 
-        Pair<Integer,Integer> pair = listMood.get(position);
+        Pair<Integer,String> pair = listMood.get(position);
         holder.display(pair);
     }
 
     public class MoodViewHolder extends RecyclerView.ViewHolder{
 
-        private final RelativeLayout backMood;
+        private final CardView backMood;
         private final ImageView imgMood;
 
      public MoodViewHolder(final View itemView) {
           super (itemView);
           imgMood=(itemView.findViewById(R.id.list_cell_image_mood_img));
-          backMood = (itemView.findViewById(R.id.list_cell_back_mood_rl));
+          backMood = (itemView.findViewById(R.id.list_cell_back_mood_cv));
      }
 
-     public void display (Pair<Integer, Integer> pair) {
+     public void display (Pair<Integer, String> pair) {
 
-       ColorDrawable cd=new ColorDrawable(pair.second);
 
        Log.d("Debug","Color :"+pair.second);
 
         imgMood.setImageResource(pair.first);
-        backMood.setBackgroundColor(pair.second);
+      //  backMood.setCardBackgroundColor(Color.parseColor("#ffde3c50"));
+         backMood.setCardBackgroundColor(Color.parseColor(pair.second));
 
      }
     };
